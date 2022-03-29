0ff93c53e86a3936c1a48801f8bb9f6b3eff9d26
Himanshu Kumar <60094122+himanshurhlk57@users.noreply.github.com>
2022-03-29 23:54:40 +0530

    Create AsyncJs.js

---
+++ PractiseNode/AsyncJs.js
@@ -0,0 +1,105 @@
+// 1 -> Callback hell
+
+function register(callback) {
+  setTimeout(() => {
+    console.log("User Registered");
+    callback();
+  }, 5000);
+}
+
+function sendEmail(callback) {
+  setTimeout(() => {
+    console.log("Welcome email sent");
+    callback();
+  }, 3000);
+}
+
+function login() {
+  setTimeout(() => {
+    console.log("User Logined");
+  }, 2000);
+}
+
+register();
+sendEmail();
+login();
+
+register(function () {
+  sendEmail(function () {
+    login();
+  });
+});
+
+console.log("other application work");
+
+// 2 -> Promise
+
+function register() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("User Registered");
+      resolve();
+    }, 5000);
+  });
+}
+
+function sendEmail() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Welcome email sent");
+      resolve();
+    }, 3000);
+  });
+}
+
+function login() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("User Logined");
+      resolve();
+    }, 2000);
+  });
+}
+
+register().then(sendEmail).then(login);
+
+console.log("other application work");
+
+// 3 -> Async Await
+
+function register() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("User Registered");
+      resolve();
+    }, 5000);
+  });
+}
+
+function sendEmail() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Welcome email sent");
+      resolve();
+    }, 3000);
+  });
+}
+
+function login() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("User Logined");
+      resolve();
+    }, 2000);
+  });
+}
+
+async function authenticate() {
+  await register();
+  await sendEmail();
+  await login();
+}
+
+authenticate();
+
+console.log("other application work");
